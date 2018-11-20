4999282da1df01367dddfed8da829a4629b3d688
mtrajvm <45171960+mtrajvm@users.noreply.github.com>
2018-11-20 17:50:40 +0000

    Add files via upload

--- src/main/java/com/qa/CVManager/Interoprability/Rest/UserRestControlller.java
+++ src/main/java/com/qa/CVManager/Interoprability/Rest/UserRestControlller.java
@@ -3,40 +3,43 @@ package com.qa.CVManager.Interoprability.Rest;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.qa.CVManager.Persistence.Domain.User;
 import com.qa.CVManager.Persistence.Respository.UserRepository;
 
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api")
 public class UserRestControlller {
 	
 	@Autowired
 	UserRepository userRepo;
-	
-		@RequestMapping(method=RequestMethod.GET)
+		
+
+		@GetMapping("/users")
 	    public Iterable<User> user() {
 	        return userRepo.findAll();
 	    }
 
-	    @RequestMapping(method=RequestMethod.POST)
+	    @PostMapping("/user")
 	    public User save(@RequestBody User user) {
 	    	userRepo.save(user);
-
 	        return user;
 	    }
 
-	    @RequestMapping(method=RequestMethod.GET, value="/{id}")
+		@GetMapping("/user/{id}")
 	    public Optional<User> show(@PathVariable String id) {
 	        return userRepo.findById(id);
 	    }
 
-	    @RequestMapping(method=RequestMethod.PUT, value="/{id}")
+		@PutMapping("/user/{id}")
 	    public User update(@PathVariable String id, @RequestBody User user) {
 	        Optional<User> optUser = userRepo.findById(id);
 	        User c = optUser.get();
@@ -50,7 +53,7 @@ public class UserRestControlller {
 	        return c;
 	    }
 
-	    @RequestMapping(method=RequestMethod.DELETE, value="/{id}")
+		@DeleteMapping("/user/{id}")
 	    public String delete(@PathVariable String id) {
 	        Optional<User> optUser = userRepo.findById(id);
 	        User user = optUser.get();
