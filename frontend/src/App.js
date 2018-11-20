24c7714038cfcf578771ee76ef578c213b4a7c9f
michal.mionskowski <michal.mionskowski95@outlook.com>
2018-11-20 15:33:26 +0000

    react fixage begin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,56 +2,25 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import UserList from './components/UserList';
+import Show from './components/Show';
+import Create from './components/Create';
+import Edit from './components/Edit';
+import Home from './components/Home';
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      users: []
-    };
-  }
-
-  componentDidMount() {
-    axios.get('/users')
-      .then(res => {
-        this.setState({ users: res.data });
-        console.log(this.state.users);
-      });
-  }
-
   render() {
     return (
-      <div class="container">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">
-              USERS LIST
-            </h3>
-          </div>
-          <div class="panel-body">
-            <h4><Link to="/create"><span class="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add User</Link></h4>
-            <table class="table table-stripe">
-              <thead>
-                <tr>
-                  <th>UserName</th>
-                  <th>Password</th>
-                  <th>AccountType</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.users.map(c =>
-                  <tr>
-                    <td><Link to={`/show/${c.id}`}>{c.userName}</Link></td>
-                    <td>{c.password}</td>
-                    <td>{c.accountType}</td>
-                  </tr>
-                )}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </div>
+      
+      <Router>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+          <Route path='/create' exact={true} component={Create}/>
+          <Route path='/Show' exact={true} component={Show}/>
+          <Route path='/ShowAll' exact={true} component={UserList}/>
+          <Route path='/edit' exact={true} component={Edit}/>
+         </Switch>
+      </Router>    
     );
   }
 }
--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -14,17 +14,22 @@ class Create extends Component {
     };
   }
   onChange = (e) => {
-    const state = this.state
-    state[e.target.userName] = e.target.value;
+    const state = this.state;
     this.setState(state);
   }
 
   onSubmit = (e) => {
     e.preventDefault();
-
     const { userName, password, accountType} = this.state;
-
-    axios.post('/users', { userName, password, accountType})
+      console.log(userName);
+      console.log(password);
+    axios.post('/api/user', { 
+      method : 'POST',
+      headers: {
+        'Content-Type' : 'application/json'
+      },
+      body: {userName, password, accountType}
+    })
       .then((result) => {
         this.props.history.push("/")
       });
@@ -37,23 +42,23 @@ class Create extends Component {
         <div class="panel panel-default">
           <div class="panel-heading">
             <h3 class="panel-title">
-              ADD USER
+              AD
             </h3>
           </div>
           <div class="panel-body">
-            <h4><Link to="/"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Users List</Link></h4>
+            <h4><Link to="/ShowAll"><span class="glyphicon glyphicon-th-list" aria-hidden="true"></span> Users List</Link></h4>
             <form onSubmit={this.onSubmit}>
               <div class="form-group">
                 <label for="isbn">User Name:</label>
-                <input type="text" class="form-control" name="userName" value={userName} onChange={this.onChange} placeholder="Name" />
+                <input type="text" class="form-control" name="userName"  onChange={this.onChange} placeholder="Name" />
               </div>
               <div class="form-group">
                 <label for="title">Password:</label>
-                <input type="text" class="form-control" name="password" value={password} onChange={this.onChange} placeholder="Password" />
+                <input type="text" class="form-control" name="password"  onChange={this.onChange} placeholder="Password" />
               </div>
               <div class="form-group">
                 <label for="author">AccountType:</label>
-                <input type="text" class="form-control" name="accountType" value={accountType} onChange={this.onChange} placeholder="AccountType" />
+                <input type="text" class="form-control" name="accountType"  onChange={this.onChange} placeholder="AccountType" />
               </div>
               <button type="submit" class="btn btn-default">Submit</button>
             </form>
