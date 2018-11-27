affc40fd648498c7a28d698263181481a3770fe2
Michal Mionskowski <michal.mionskowski95@outlook.com>
2018-11-27 01:59:12 +0000

    pdf displaying

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import UserList from './components/UserList';
 import Show from './components/Show';
 import Create from './components/Create';
 import Edit from './components/Edit';
 import Home from './components/Home';
+import AppNavbar from './components/AppNavbar';
+import Login from './components/Login';
+import Register from './components/Register';
+import List from './components/List';
+
 class App extends Component {
   render() {
     return (
-      
       <Router>
         <Switch>
-          <Route path='/' exact={true} component={Home}/>
+          <Route path='/' exact={true} component={Login}/>
+          <Route path='/Register' exact={true} component={Register}/>
+          <Route path='/home' exact={true} component={Home}/>
           <Route path='/create' exact={true} component={Create}/>
-          <Route path='/Show' exact={true} component={Show}/>
+          <Route path='/Show/:id' exact={true} component={Show}/>
           <Route path='/ShowAll' exact={true} component={UserList}/>
-          <Route path='/edit' exact={true} component={Edit}/>
+          <Route path='/edit/:id' exact={true} component={Edit}/>
+          <Route path='/List' exact={true} component={List}/>
          </Switch>
-      </Router>    
+      </Router>  
     );
   }
 }
